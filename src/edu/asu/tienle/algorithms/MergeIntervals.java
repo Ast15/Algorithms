8adca2e5162a2ad4ac5d2ebb6d7c8c8b488281a1
Ast15 <tienttt10@gmail.com>
2014-04-19 20:48:35 -0700

    Updated
    With problem descriptions

--- src/edu/asu/tienle/algorithms/MergeIntervals.java
+++ src/edu/asu/tienle/algorithms/MergeIntervals.java
@@ -6,6 +6,14 @@ import java.util.Comparator;
 
 
 /**
+ * Problem from: http://oj.leetcode.com/problems/merge-intervals/
+
+	Given a collection of intervals, merge all overlapping intervals.
+	For example,
+	Given [1,3],[2,6],[8,10],[15,18],
+	return [1,6],[8,10],[15,18].
+ * 
+ * 
  * Definition for an interval.
  * public class Interval {
  *     int start;
